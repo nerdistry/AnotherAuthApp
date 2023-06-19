061a16afaddc0c87c8e1acc957d6835db1db5514
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-06-20 01:53:14 +0300

    Debugging error.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Signup from "./components/Signup"
+import { Routes, Route, Router } from 'react-router-dom'
+import Dashboard from "./components/Dashboard"
+import Login from "./components/Login"
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/ForgotPassword"
+import UpdateProfile from "./components/UpdateProfile"
+import { AuthContextProvider } from './contexts/AuthContext'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <Router>
+
+      <AuthContextProvider>
+
+      <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/" element={<PrivateRoute />}/>
+      <Route index element={<Dashboard />} />
+      <Route path="/update-profile" element={<UpdateProfile />} />
+      </Routes>
+
+    </AuthContextProvider>
+    
+    </Router>
+
+  )
 }
 
 export default App;
